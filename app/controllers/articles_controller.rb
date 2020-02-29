366590e553aec13b8c1e222eac9bfc47409d0cf4
Rafael <fazylovraf@gmail.com>
2020-02-29 17:59:54 +0300

    hey

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,20 +11,34 @@ class ArticlesController < ApplicationController
 		@article = Article.new
 	end
 
-	def create
-	  @article = Article.new(article_params)
-
-	  if @article.save
-	    redirect_to @article
-	  else
-	    render 'new'
-	  end
+	def edit
+  		@article = Article.find(params[:id])
 	end
 
-	private
-	  def article_params
-	    params.require(:article).permit(:title, :text)
-	  end
+	def create
+  @article = Article.new(article_params)
+
+  if @article.save
+    redirect_to @article
+  else
+    render 'new'
+  end
+end
+
+def update
+  @article = Article.find(params[:id])
+
+  if @article.update(article_params)
+    redirect_to @article
+  else
+    render 'edit'
+  end
+end
+
+private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
   	 
 end
 
